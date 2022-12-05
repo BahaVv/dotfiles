0212cdf1f3faa9db102bc911b26c255e13820f62
Adam Kimball <baha@astyr.net>
2022-12-05 14:24:27 -0500

    Update server core pinning

--- scripts/docker/tf2-server.sh
+++ scripts/docker/tf2-server.sh
@@ -1,1 +1,1 @@
-docker run -d --net=host -v $HOME/.tf2/tf2-data:/home/steam/tf-dedicated/ --cpuset-cpus="19" --restart unless-stopped --name=tf2-server -e SRCDS_TOKEN=$TF2_TOKEN -e SRCDS_MAXPLAYERS=24 -e SRCDS_PW=$TF2_PW -e SRCDS_HOSTNAME="A Very Purple TF2 Server" -it cm2network/tf2
+docker run -d --net=host -v $HOME/.tf2/tf2-data:/home/steam/tf-dedicated/ --cpuset-cpus="31" --restart unless-stopped --name=tf2-server -e SRCDS_TOKEN=$TF2_TOKEN -e SRCDS_MAXPLAYERS=24 -e SRCDS_PW=$TF2_PW -e SRCDS_HOSTNAME="A Very Purple TF2 Server" -it cm2network/tf2
